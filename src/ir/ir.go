71316db0414be8b80566261c8a5e71f432411177
specctrum <unkn.spectrum@gmail.com>
2023-04-22 14:05:31 +0700

    1

--- src/ir/ir.go
+++ src/ir/ir.go
@@ -1,1 +1,29 @@
 package ir
+
+/* Programm IR structure
+.Filename: test
+.Constants
+	alpha int 12
+	beta int 12
+.Functions
+; a:int, b:int -> int
+_main:
+	hlt
+*/
+
+type Program struct {
+	Filename  string
+	Constants []Constant
+	Functions []Function
+}
+
+type Constant struct {
+	Symbol string
+	Value  Object
+}
+
+type Function struct {
+	Symbol string
+	Args   []Argument
+	ReturnType
+}
---
+++ src/object/object.go
@@ -0,0 +1,5 @@
+package object
+
+type Object interface {
+	ObjectType()
+}
