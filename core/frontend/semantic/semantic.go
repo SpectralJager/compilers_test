3c10ccfec807947b10cfbf7d6f68c84c1499beb8
specctrum <unkn.spectrum@gmail.com>
2023-05-19 15:56:42 +0700

    1

---
+++ core/frontend/semantic/semantic.go
@@ -0,0 +1,33 @@
+package semantic
+
+// Scopes
+type Scope struct{}
+
+/*
+global pkgName |>
+	#symbolTable:
+		fn add: ...int -> int
+		fn sub: ...int -> int
+		fn mul: ...int -> int
+		fn div: ...int -> int
+		fn printf: format ...strings -> void
+		fn main: void -> void
+		const a:int
+		var b:int
+	function main |>
+		#symbolTable:
+			const a:int
+			const b:int
+		local ifBody_{sometoken1}
+		local elifBody_{sometoken1}_1
+		local elifBody_{sometoken1}_2
+		local elseBody_{sometoken1}
+		local whileBody_{sometoken2}
+		local elseBody_{sometoken2}
+		local forBody_{sometoken3} |>
+			#symbolTable:
+				var i:int
+*/
+
+// SYmbol table
+type SymbolTable struct{}
