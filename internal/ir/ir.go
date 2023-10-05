151d117664269b8a29264c2bf29e39538fd1ad3b
spectrum <unkn.spectrum@gmail.com>
2023-10-05 21:30:19 +0700

    1

--- internal/ir/ir.go
+++ internal/ir/ir.go
@@ -1,16 +1,47 @@
 package ir
 
 /*
-types |>
-	int -> INT
-	bool -> BOOL
-init |>
-	call main
-functions |>
-func:
+module:
+	name -> ...
+	path -> ...
+	imports |>
+		"..." as ...
+global |>
+	block.begin global
 	var.new a int
-	var.pop a
-	block.begin
-	var.new a int
+	var.set a 12
+	call main 0
+	block.end
+main |>
+	block.begin fn
+	var.new res int
+	const.push 40
+	call fib 1
+	var.pop res
+	var.push res
+	callb printf 1
+	block.end
+fib |>
+	block.begin fn
+	var.new n int
+	var.pop n
+	var.push n
+	const.push 2
+	callb int/lt 2
+	block.begin local
+	var.push n
+	stack.type int
+	ret 1
+	block.end
+	var.push n
+	const.push 1
+	callb int/sub 2
+	var.push n
+	const.push 2
+	callb int/sub 2
+	callb int/add 2
+	call fib 1
+	stack.type int
+	ret 1
 	block.end
 */
