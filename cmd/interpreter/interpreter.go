559966538816fd37e1f26be048f02283e42ef0b4
spectrum <unkn.spectrum@gmail.com>
2024-01-02 14:09:45 +0700

    1

--- cmd/interpreter/interpreter.go
+++ cmd/interpreter/interpreter.go
@@ -8,6 +8,7 @@ import (
 	"grimlang/eval"
 	"log"
 	"os"
+	"runtime/pprof"
 
 	"github.com/alecthomas/participle/v2"
 )
@@ -17,6 +18,12 @@ func main() {
 		fmt.Printf("usage: grim <filename>")
 		os.Exit(1)
 	}
+
+	f, err := os.Create("prof.prof")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	file, err := os.ReadFile(os.Args[1])
 	if err != nil {
 		log.Fatalf("something goes wrong -> %s", err.Error())
@@ -27,9 +34,11 @@ func main() {
 		fmt.Printf("%s\n", errBuf.String())
 		log.Fatalf("something goes wrong -> %s", err.Error())
 	}
+	pprof.StartCPUProfile(f)
 	builtinContext := context.NewBuiltinContext()
 	if err := new(eval.EvalState).EvalModule(builtinContext, module); err != nil {
 		log.Fatalf("something goes wrong -> %s", err.Error())
 	}
+	pprof.StopCPUProfile()
 	os.Exit(0)
 }
