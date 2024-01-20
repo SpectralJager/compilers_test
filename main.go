96dca726669f9404815cff21faf64a3300150181
spectrum <unkn.spectrum@gmail.com>
2024-01-20 21:49:02 +0700

    1

--- main.go
+++ main.go
@@ -1,31 +1,4 @@
 package main
 
-import (
-	"bytes"
-	"fmt"
-	"grimlang/ast"
-	"grimlang/context"
-	"grimlang/eval"
-	"log"
-	"os"
-
-	"github.com/alecthomas/participle/v2"
-)
-
 func main() {
-	file, err := os.ReadFile("examples/test.grim")
-	if err != nil {
-		log.Fatalf("something goes wrong -> %s", err.Error())
-	}
-	var errBuf bytes.Buffer
-	module, err := ast.Parser.ParseBytes("", file, participle.Trace(&errBuf))
-	if err != nil {
-		fmt.Printf("%s\n", errBuf.String())
-		log.Fatalf("something goes wrong -> %s", err.Error())
-	}
-	builtinContext := context.NewBuiltinContext()
-	if err := new(eval.EvalState).EvalModule(builtinContext, module); err != nil {
-		log.Fatalf("something goes wrong -> %s", err.Error())
-	}
-	os.Exit(0)
 }
--- runtime/runtime.go
+++ runtime/runtime.go
@@ -1,109 +1,111 @@
 package runtime
 
+/*
+-----------<Enviroment
+builtin |>
+
+	int -> import(int)
+	main -> import(main)
+	exit -> fn[int] <void>
+	...
+
+int |>
+
+	add -> fn[...int] <int>
+	sub -> fn[...int] <int>
+	...
+
+main::builtin|>
+
+	vector -> import(hash1)
+	main -> fn[] <void>
+
+hash1::builtin|>
+
+	point -> import(hash2)
+	vector -> record<p0::point/point p1::point/point>
+	new_vector -> fn[point/point point/point] <vector>
+	...
+
+hash2::builtin|>
+
+	point -> record<x0::int x1::int>
+	new_point -> fn[int int] <point>
+	...
+*/
 type Kind uint
 
 const (
-	Undefined Kind = iota
+	Invalid Kind = iota
 
-	Variable // variable symbol
-	Constant // constant symbol
-	Module   // module symbol
-	Function // function symbol, type and litteral
-	Record   // record symbol, type and litteral
-	Void     // void type
-	Any      // any type
-	Null     // null type and litteral
-	Int      // int type and litteral
-	Float    // float type and litteral
-	String   // string type and litteral
-	Bool     // bool type and litteral
-	List     // list type and litteral
-	Builtin  // builtin function litteral
+	ImportSymbol
+	VariableSymbol
+	ConstantSymbol
+	FunctionSymbol
+	RecordSymbol
+
+	IntLitteral
+	RecordLitteral
+	FunctionLitteral
+	BuiltinLitteral
+
+	VoidType
+	IntType
+	RecordType
+	FunctionType
 )
 
-var kindNames = map[Kind]string{
-	Undefined: "undefined",
-	Variable:  "var",
-	Constant:  "const",
-	Module:    "module",
-	Function:  "function",
-	Record:    "record",
-	Void:      "void",
-	Any:       "any",
-	Null:      "null",
-	Int:       "int",
-	Float:     "float",
-	String:    "string",
-	Bool:      "bool",
-	List:      "list",
-	Builtin:   "builtin",
-}
-
-func (kind Kind) String() string {
-	name, ok := kindNames[kind]
-	if !ok {
-		return kindNames[Undefined]
-	}
-	return name
-}
-
-type Type interface {
-	// general methods for all types
-	Kind() Kind        // get kind of type
-	Name() string      // get name of type
-	Compare(Type) bool // compare types
+type Enviroment interface {
 	String() string
-	// methods for specific types
-	Subtype() Type // get list's items or null item type
-	// function methods
-	NumIn() int  // get number of ins
-	In(int) Type // get function's i'th argument type
-	Out() Type   // get function's return type
-	// record methods
-	NumField() int   // get number of record's fields
-	Field(int) Field // get record's field by index
-	FieldIndex(string) int
-	FieldByName(string) Field // get record's field by name
+	Tag() string
+	Parent() Enviroment
+	Search(string) Symbol
+	SearchLocal(string) Symbol
+
+	Insert(Symbol) error
 }
 
-type Field interface {
+type Symbol interface {
+	String() string
+	Kind() Kind
 	Name() string
 	Type() Type
+	Value() Litteral
+	Tag() string
+
+	Set(Litteral) error
 }
 
 type Litteral interface {
-	Kind() Kind
 	String() string
+	Kind() Kind
 	Type() Type
-
 	Int() int64
-	Float() float64
-	Bool() bool
-	Str() string
-
-	Item(int) Litteral
-	Len() int
-
-	Call(Context, ...Litteral) (Litteral, error)
-
 	Field(int) Litteral
 	FieldByName(string) Litteral
 }
 
-type Symbol interface {
+type Type interface {
+	String() string
 	Kind() Kind
 	Name() string
-	String() string
-
-	Type() Type
-	Value() Litteral
-	Set(Litteral) error
+	NumIns() int
+	In(int) Type
+	Out() Type
+	Field(int) Field
+	FieldByName(string) Field
+	FieldIndex(string) int
+	NumFields() int
 }
 
-type Context interface {
+type Field interface {
 	String() string
-	Scope() string
-	Search(string) Symbol
-	Insert(Symbol) error
-	Update(Symbol) error
+	Name() string
+	Type() Type
+}
+
+type enviroment struct {
+	parent  Enviroment
+	tag     string
+	symbols map[string]Symbol
 }
